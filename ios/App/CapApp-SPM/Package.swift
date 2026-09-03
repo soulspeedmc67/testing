484d00ea932003fe1c714bed6163ed68685923b7
AleemKanyu <aleemkanyu@gmail.com>
2026-09-03 12:28:17 +0530

    ci(ios): patch Swift Package Manager targets with NonescapableTypes for Capacitor 8.5

--- ios/App/CapApp-SPM/Package.swift
+++ ios/App/CapApp-SPM/Package.swift
@@ -19,6 +19,7 @@ let package = Package(
     targets: [
         .target(
             name: "CapApp-SPM",
+            swiftSettings: [.enableExperimentalFeature("NonescapableTypes")],
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
                 .product(name: "Cordova", package: "capacitor-swift-pm"),
